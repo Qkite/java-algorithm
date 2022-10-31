3b7c3aa4fda92cfa826e409b531b8dc6dc31bf86
Qkite <yeonjigu4@gmail.com>
2022-10-31 09:31:44 +0900

    모의고사

---
+++ src/main/java/com/likelion/algorithm1031/MockExam.java
@@ -0,0 +1,16 @@
+package com.likelion.algorithm1031;
+
+public class MockExam {
+
+    /*
+    1번: 5번씩 반복
+    2번: 10번씩 반복
+    --> 반복되는 횟수를 기준으로 나머지를 이용해서 풀이?
+
+     */
+
+    public int[] solution(int[] answers) {
+    int[] answer = {};
+    return answer;
+    }
+}
---
+++ src/test/java/com/likelion/algorithm1031/MockExamTest.java
@@ -0,0 +1,16 @@
+package com.likelion.algorithm1031;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MockExamTest {
+
+    @Test
+    void test1(){
+        MockExam mockExam = new MockExam();
+        mockExam.solution(int[] {1,2,3,4,5});
+
+    }
+
+}
