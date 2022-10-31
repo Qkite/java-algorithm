841be73bf37659bdd915ff7b57cca2f007450393
Qkite <yeonjigu4@gmail.com>
2022-10-31 09:44:03 +0900

    모의고사 - 메소드 설계

--- src/main/java/com/likelion/algorithm1031/MockExam.java
+++ src/main/java/com/likelion/algorithm1031/MockExam.java
@@ -1,5 +1,7 @@
 package com.likelion.algorithm1031;
 
+import java.util.ArrayList;
+
 public class MockExam {
 
     /*
@@ -12,20 +14,27 @@ public class MockExam {
 
     public int[] solution(int[] answers) {
         int[] answer = {};
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int cnt3 = 0;
+        ArrayList<Integer> count = new ArrayList<>(3);
 
         // 10000
 
         String[] answer1 = "1,2,3,4,5".repeat(10000/5).split(",");
-        String[] answer2 = "2,1,2,3,2,4,2,5".repeat(10000/5).split(",")
+        String[] answer2 = "2,1,2,3,2,4,2,5".repeat(10000/8).split(",");
+        String[] answer3 = "3,3,1,1,2,2,4,4,5,5".repeat(10000/10).split(",");
 
         for (int i = 0; i < answer.length; i++) {
-
-
-
+            if(answer1[i].equals(answer[i])){
+                count.add([0]++;
+            }
+            if(answer2[i].equals(answer[i])){
+                count[1]++;
+            }
+            if(answer3[i].equals(answer[i])){
+                count[2]++;
+            }
         }
+
+        Math.max(Math.max(count[0], count[1]),count[2])
 
 
         return answer;
