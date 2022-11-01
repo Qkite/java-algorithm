99062f086ec28b107eae5e9042b910e7f104d1ff
Qkite <yeonjigu4@gmail.com>
2022-11-01 10:12:58 +0900

    template callback을 적용해서 소수 구하기

--- src/main/java/com/likelion/algorithm1101/IsITPrimeNumber.java
+++ src/main/java/com/likelion/algorithm1101/IsITPrimeNumber.java
@@ -1,45 +1,68 @@
 package com.likelion.algorithm1101;
 
-public class IsITPrimeNumber {
+interface InsertCondition {
+    public int insertCondition();
+}
 
-    public boolean solution1(int input) {
+class PrimeNumberCalculation{
+
+    boolean solution(int input, int condition) {
+
+
         boolean answer=true;
 
         // 2부터 n-1까지
-        for (int i = 2; i < input; i++) {
+        for (int i = 2; i <= condition; i++) {
             if(input % i == 0){
                 answer = false;
             }
         }
 
+        return answer;
+    }
+
+    private InsertCondition insertToLoop(int condition) {
+
+        return new InsertCondition() {
+            @Override
+            public int insertCondition() {
+
+                return condition;
+            }
+        };
+
+    }
+
+
+}
+
+public class IsITPrimeNumber {
+
+
+
+    public boolean solution1(int input) {
+        PrimeNumberCalculation primeNumberCalculation = new PrimeNumberCalculation();
+        boolean answer = primeNumberCalculation.solution(input, input-1);
         return answer;
     }
 
     public boolean solution2(int input) {
-        boolean answer=true;
-
-        // 2부터 루트 n 까지
-        for (int i = 2; i < Math.ceil(Math.pow(input, 0.5)); i++) {
-            if(input % i == 0){
-                answer = false;
-            }
-        }
-
+        PrimeNumberCalculation primeNumberCalculation = new PrimeNumberCalculation();
+        boolean answer = primeNumberCalculation.solution(input, (int) Math.ceil(Math.pow(input, 0.5)));
         return answer;
     }
 
     public boolean solution3(int input) {
-        boolean answer=true;
-
-        // 2부터 루트 n 까지
-        for (int i = 2; i < input/2; i++) {
-            if(input % i == 0){
-                answer = false;
-            }
-        }
-
+        PrimeNumberCalculation primeNumberCalculation = new PrimeNumberCalculation();
+        boolean answer = primeNumberCalculation.solution(input, input/2);
         return answer;
     }
+
+//
+//    // 에라토스테네스의 체
+//    public boolean solution4(int input){
+//
+//    }
 
 
 }
