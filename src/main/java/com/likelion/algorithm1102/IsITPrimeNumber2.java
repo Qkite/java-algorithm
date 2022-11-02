ce580fa4954246ad56be27b2a2fc0f2ccc2e55a6
Qkite <yeonjigu4@gmail.com>
2022-11-02 10:35:32 +0900

    에라토스테네스 체 - check array 사용

--- src/main/java/com/likelion/algorithm1102/IsITPrimeNumber2.java
+++ src/main/java/com/likelion/algorithm1102/IsITPrimeNumber2.java
@@ -28,17 +28,36 @@ public class IsITPrimeNumber2 {
         return numList.size();
     }
 
-    // 어떻게 하면 이중 for문을 없앨 수 있을까...?
+    // remove를 하면 그 원소를 지우고 배열을 다시 생성 --> add와 remove를 사용하지 않는 방향으로 가는 것이 좋다
+    // 배수인지를 나타내는 check 배열을 사용하는 것이 좋음
 
     public int solution2(int N){
 
         int[] numArray = new int[N-1];
+        boolean[] isItCompositionNumber = new boolean[N-1];
 
         for (int i = 2; i <= N; i++) {
             numArray[i-2] = i;
         }
 
-        return 0;
+        int element = (int) Math.floor(Math.pow(N,0.5));
+
+
+        for (int i = 0; i < element; i++) {
+            for (int j = i; j < numArray.length; j++) {
+                if(numArray[j] %numArray[i] ==0 ){
+                    isItCompositionNumber[j] = true;
+                }
+            }
+        }
+        int sum = 0;
+
+        for (int i = 0; i < numArray.length; i++) {
+            sum += numArray[i];
+
+        }
+
+        return N-1-sum;
     }
 
 
