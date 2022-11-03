0e0eea04f0b180d342f8e5b7fa120be8f393ed65
Qkite <yeonjigu4@gmail.com>
2022-11-03 09:48:22 +0900

    에레테네스체 - check 배열 이용해 소수 개수 찾기 - 오류 수정

--- src/main/java/com/likelion/algorithm1102/IsITPrimeNumber3.java
+++ src/main/java/com/likelion/algorithm1102/IsITPrimeNumber3.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public class IsITPrimeNumber3 {
 
+    // % 연산을 하지 않는 것
+
 
     // 2: 2,4,6,... -> 초항: 2*2-2, 공차: 2
     // 3: 4,7,10,... --> 초항: 3*2-2 공차: 3
@@ -21,22 +23,27 @@ public class IsITPrimeNumber3 {
 
 
     public static void main(String[] args) {
-        int N = 1000000;
-        boolean[] checkarray = new boolean[N];
+        int N = 5;
+        boolean[] checkarray = new boolean[N-1];
+        // 주의1) 배열이 2부터 시작함 -> 총 N-1개
 
         for (int i = 2; i*i <= N; i++) {
-            for (int j = 2*i-2; j <N ; j = j+i) {
+            for (int j = 2*i-2; j <N-1 ; j = j+i) {
                 checkarray[j] = true;
             }
-
         }
+
+        // 주의2) index가 2의 index가 0이므로 N-2
+        // for문에서는 j<N-1로 정지조건을 설정해야함
 
         int count = 0;
 
-        for (boolean status:checkarray) {
-            if(status == false){
+        for (int i = 0; i < checkarray.length; i++) {
+            if(checkarray[i]==false){
+                System.out.println(i);
                 count++;
             }
+
         }
         System.out.println(count);
 
