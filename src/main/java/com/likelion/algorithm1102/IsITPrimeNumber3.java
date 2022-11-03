3bae8a50a6b2b9677e9c31b1fa54ac3119807679
Qkite <yeonjigu4@gmail.com>
2022-11-03 09:35:59 +0900

    에레테네스체 - check 배열 이용해서 100만까지의 소수 개수 찾기

--- src/main/java/com/likelion/algorithm1102/IsITPrimeNumber3.java
+++ src/main/java/com/likelion/algorithm1102/IsITPrimeNumber3.java
@@ -7,16 +7,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class IsITPrimeNumber3 {
-    // 2: 2,4,6,...
-    // 3: 4,7,10,...
+
+
+    // 2: 2,4,6,... -> 초항: 2*2-2, 공차: 2
+    // 3: 4,7,10,... --> 초항: 3*2-2 공차: 3
     // 4: 6,10,14,...
     // 5: 8,13,18,...
     // 6: 10,16,22...
     // 7: 12,19,26,...
+    // --> ni-2 i는 2부터 시작
+
+
+
 
     public static void main(String[] args) {
-        for (int i = 2; i <= 100; i++) {
-            System.out.printf("%d %d\n", i, (i-1)*2);
+        int N = 1000000;
+        boolean[] checkarray = new boolean[N];
+
+        for (int i = 2; i*i <= N; i++) {
+            for (int j = 2*i-2; j <N ; j = j+i) {
+                checkarray[j] = true;
+            }
+
         }
+
+        int count = 0;
+
+        for (boolean status:checkarray) {
+            if(status == false){
+                count++;
+            }
+        }
+        System.out.println(count);
+
     }
 }
