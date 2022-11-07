4caa64bdeb580ecb44dc5e3eb22ceb0894845da6
Qkite <yeonjigu4@gmail.com>
2022-11-07 10:36:41 +0900

    나누어떨어지는숫자배열 - 우선순위큐 이용하기 - [1,3,2,36]이 나오는 이유가 뭘까?

--- src/main/java/com/likelion/algorithm1107/MakeArrWithDivisor.java
+++ src/main/java/com/likelion/algorithm1107/MakeArrWithDivisor.java
@@ -59,34 +59,89 @@ public class MakeArrWithDivisor {
         PriorityQueue<Integer> answer = new PriorityQueue<>();
 
         for (int num:arr) {
-            if(num %divisor == 0){
+            if(num % divisor == 0){
                 answer.add(num);
+                System.out.println(answer);
             }
         }
 
         if(answer.size() == 0){
             answer.add(-1);
+            System.out.println(answer);
         }
 
+        // 왜 priorityque를 그대로 print하면 1,3,2,36이 나올까?
 
         return answer;
+    }
+
+    public int[] solution4(int[] arr, int divisor) {
+
+        PriorityQueue<Integer> answer = new PriorityQueue<>();
+
+        for (int num:arr) {
+            if(num % divisor == 0){
+                answer.add(num);
+
+            }
+        }
+
+        if(answer.size() == 0){
+            answer.add(-1);
+
+        }
+
+        // list를 Array로 바꾸고
+        int[] answer1 = new int[answer.size()];
+        int idx = 0;
+        while(!answer.isEmpty()){
+            answer1[idx++] = answer.poll();
+        }
+        return answer1;
     }
 
 
 
     public static void main(String[] args) {
        MakeArrWithDivisor makeArrWithDivisor = new MakeArrWithDivisor();
-       System.out.println(makeArrWithDivisor.solution1(new int[]{5,9,7,10}, 5));
-       System.out.println(makeArrWithDivisor.solution1(new int[]{2,36,1,3}, 1));
-       System.out.println(makeArrWithDivisor.solution1(new int[]{3,2,6}, 10));
+//       System.out.println(makeArrWithDivisor.solution1(new int[]{5,9,7,10}, 5));
+//       System.out.println(makeArrWithDivisor.solution1(new int[]{2,36,1,3}, 1));
+//       System.out.println(makeArrWithDivisor.solution1(new int[]{3,2,6}, 10));
 
-       System.out.println(Arrays.toString(makeArrWithDivisor.solution2(new int[]{5, 9, 7, 10}, 5)));
-       System.out.println(Arrays.toString(makeArrWithDivisor.solution2(new int[]{2, 36, 1, 3}, 1)));
-       System.out.println(Arrays.toString(makeArrWithDivisor.solution2(new int[]{3, 2, 6}, 10)));
+//       System.out.println(Arrays.toString(makeArrWithDivisor.solution2(new int[]{5, 9, 7, 10}, 5)));
+//       System.out.println(Arrays.toString(makeArrWithDivisor.solution2(new int[]{2, 36, 1, 3}, 1)));
+//       System.out.println(Arrays.toString(makeArrWithDivisor.solution2(new int[]{3, 2, 6}, 10)));
 
         System.out.println(makeArrWithDivisor.solution3(new int[]{5, 9, 7, 10}, 5));
+
+        /*
+        [5]
+        [5, 10]
+        [5, 10]
+         */
+
         System.out.println(makeArrWithDivisor.solution3(new int[]{2, 36, 1, 3}, 1));
+        // 왜 1,3,2,36이 나올까?
+
+        /*
+        [2]
+        [2, 36]
+        [1, 36, 2]
+        [1, 3, 2, 36]
+        [1, 3, 2, 36]
+         */
+
         System.out.println(makeArrWithDivisor.solution3(new int[]{3, 2, 6}, 10));
+
+        /*
+        [-1]
+        [-1]
+         */
+
+//        System.out.println(Arrays.toString(makeArrWithDivisor.solution4(new int[]{5, 9, 7, 10}, 5)));
+//        System.out.println(Arrays.toString(makeArrWithDivisor.solution4(new int[]{2, 36, 1, 3}, 1)));
+//        System.out.println(Arrays.toString(makeArrWithDivisor.solution4(new int[]{3, 2, 6}, 10)));
+
 
 
 
