512816866846002a4b2807fdd6bbff7f85fa5684
Qkite <yeonjigu4@gmail.com>
2022-11-08 10:19:22 +0900

    이진 탐색 - int[] 이용하기

---
+++ src/main/java/com/likelion/algorithm1108/BinarySearch.java
@@ -0,0 +1,36 @@
+package com.likelion.algorithm1108;
+
+public class BinarySearch {
+
+    public boolean getSolution(int[] nums, int num){
+        int firstIdx = 0;
+        int lastIdx = nums.length-1;
+        int midIdx = (firstIdx + lastIdx)/2;
+
+        while(lastIdx - firstIdx>1){
+            // lastIdx > firstIdx로 하면 index 차이가 1개인 경우 index가 업데이트가 되지 않음 -> 무한 루프
+            // 따라서 차이가 2 이상 나는 경우에만 index를 업데이트 하도록 함
+            if(num < nums[midIdx]){
+                lastIdx = midIdx;
+            } else{
+                firstIdx = midIdx;
+            }
+
+            midIdx = (firstIdx + lastIdx)/2;
+
+        }
+
+        return num == nums[midIdx];
+
+    }
+
+    public static void main(String[] args) {
+        BinarySearch binarySearch = new BinarySearch();
+        System.out.println(binarySearch.getSolution(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13}, 3));
+        System.out.println(binarySearch.getSolution(new int[]{1,3,4,5,6,7,9,10,11,13,15,16}, 12));
+        System.out.println(binarySearch.getSolution(new int[]{1,3,4,5,6,7,9,10,11,13,15,16,22,21,25,28}, 23));
+
+
+    }
+
+}
--- src/main/java/com/likelion/algorithm1108/SimpleSearch.java
+++ src/main/java/com/likelion/algorithm1108/SimpleSearch.java
@@ -3,10 +3,9 @@ package com.likelion.algorithm1108;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class SimpleSearch {
 
