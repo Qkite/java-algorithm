53b1cb64aff3e4230f75f6266c7df35b9238472e
Qkite <yeonjigu4@gmail.com>
2022-11-08 09:24:29 +0900

    코드업 - 데이터 탐색, 리스트 이용

---
+++ src/main/java/com/likelion/algorithm1108/SimpleSearch.java
@@ -0,0 +1,42 @@
+package com.likelion.algorithm1108;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class SimpleSearch {
+
+    public void getSolution(List<String> caseList, String forFind){
+
+        int index = caseList.indexOf(forFind);
+
+        if( index> -1){
+            System.out.println(index+1);
+        } else{
+            System.out.println(index);
+        }
+
+
+
+    }
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String caseNum = br.readLine();
+
+        String[] cases = br.readLine().split(" ");
+        String whatWeNeedFind = br.readLine();
+        List<String> caseList = Arrays.asList(cases);
+
+        SimpleSearch simpleSearch = new SimpleSearch();
+        simpleSearch.getSolution(caseList, whatWeNeedFind);
+
+    }
+}
