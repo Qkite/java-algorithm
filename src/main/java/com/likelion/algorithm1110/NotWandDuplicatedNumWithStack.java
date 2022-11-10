d59bb21df322d7929cf2c000d3099d80571a4705
Qkite <yeonjigu4@gmail.com>
2022-11-10 09:32:11 +0900

    같은 숫자는 싫어 - stack 이용해서 구현

---
+++ src/main/java/com/likelion/algorithm1110/NotWandDuplicatedNumWithStack.java
@@ -0,0 +1,32 @@
+package com.likelion.algorithm1110;
+
+import java.util.Stack;
+
+public class NotWandDuplicatedNumWithStack {
+
+    public Stack<Integer> solution(int[] arr){
+
+        Stack<Integer> answer = new Stack<>();
+        answer.push(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if(answer.peek() == arr[i]){
+                continue;
+            } else{
+                answer.push(arr[i]);
+            }
+
+        }
+
+        return answer;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        NotWandDuplicatedNumWithStack notWandDuplicatedNumWithStack = new NotWandDuplicatedNumWithStack();
+        System.out.println(notWandDuplicatedNumWithStack.solution(new int[] {1,3,5,4,4,1,6,5,1,3,2,1,1,2,2}));
+
+    }
+}
