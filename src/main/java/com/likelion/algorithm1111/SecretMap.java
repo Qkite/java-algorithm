69ba1de44afbefaae603fddb4655aa3098e5687e
Qkite <yeonjigu4@gmail.com>
2022-11-11 09:11:37 +0900

    비밀지도 - 어떻게 풀지 구상

---
+++ src/main/java/com/likelion/algorithm1111/SecretMap.java
@@ -0,0 +1,17 @@
+package com.likelion.algorithm1111;
+
+public class SecretMap {
+    // toBinaryString을 통해서 이진수로 변환
+    // 두 지도를 겹쳤을 때 둘다 0이어야 0 --> 더해서 0이면 " "
+    // 아니면 "#"
+    // charAt + for문
+
+
+
+    public static void main(String[] args) {
+
+        String binaryString = Integer.toBinaryString(9);
+
+    }
+
+}
