ddfc2b76d2bb8c0474424da3e506c26c09005f97
Qkite <yeonjigu4@gmail.com>
2022-11-16 10:48:15 +0900

    퀵 정렬 - Array 이용 - stack over flow...

--- src/main/java/com/likelion/algorithm1115/QuickSort2.java
+++ src/main/java/com/likelion/algorithm1115/QuickSort2.java
@@ -5,57 +5,49 @@ import java.util.Arrays;
 public class QuickSort2 {
 
     int[] sort(int[] arr) {
-        int n = 0;
 
-        // 중앙값이 고정되는 정지 조건이 뭘까...?
-
-        while(n<5){
-            int leftIdx = 0;
-            int rightIdx = arr.length - 1;
-            int pivotIdx = arr.length / 2;
-            while (leftIdx < pivotIdx) {
-                if (arr[pivotIdx] <= arr[leftIdx]) {
-                    int temp = arr[pivotIdx];
-                    arr[pivotIdx] = arr[leftIdx];
-                    arr[leftIdx] = temp;
-                    pivotIdx--;
-                    leftIdx++;
-                } else {
-                    leftIdx++;
-
-                }
-            }
-
-            while (leftIdx <= rightIdx) {
-                if (arr[rightIdx] <= arr[leftIdx]) {
-                    int temp = arr[rightIdx];
-                    arr[rightIdx] = arr[leftIdx];
-                    arr[leftIdx] = temp;
-                    rightIdx--;
-                    leftIdx++;
-                } else {
-                    rightIdx--;
-                }
+        int leftIdx = 0;
+        int rightIdx = arr.length - 1;
+        int pivotIdx = arr.length / 2;
+        while (leftIdx < pivotIdx) {
+            if (arr[pivotIdx] <= arr[leftIdx]) {
+                int temp = arr[pivotIdx];
+                arr[pivotIdx] = arr[leftIdx];
+                arr[leftIdx] = temp;
+                pivotIdx--;
+                leftIdx++;
+            } else {
+                leftIdx++;
 
             }
-            n++;
         }
 
+        while (leftIdx <= rightIdx) {
+            if (arr[rightIdx] <= arr[leftIdx]) {
+                int temp = arr[rightIdx];
+                arr[rightIdx] = arr[leftIdx];
+                arr[leftIdx] = temp;
+                rightIdx--;
+                leftIdx++;
+            } else {
+                rightIdx--;
+            }
+
+        }
+
+        int[] left = sort(Arrays.copyOfRange(arr, 0, pivotIdx));
+        int[] right = sort(Arrays.copyOfRange(arr, pivotIdx, arr.length));
 
 
+        for (int i = 0; i < arr.length; i++) {
+            if(i<left.length){
+                arr[i] = left[i];
+            } else{
+                arr[i] = right[i-left.length];
+            }
 
-//        int[] left = sort(Arrays.copyOfRange(arr, 0, pivotIdx));
-//        int[] right = sort(Arrays.copyOfRange(arr, pivotIdx, arr.length));
-//        int[] result = new int[arr.length];
-//        for (int i = 0; i < result.length; i++) {
-//            if(i<left.length){
-//                result[i] = left[i];
-//            } else{
-//                result[i] = right[i-left.length];
-//            }
-//
-//        }
-//
+        }
+
         return arr;
     }
 
