9a6dab96bf452602bf1ee797a37966ec7ce14692
Qkite <yeonjigu4@gmail.com>
2022-11-22 09:16:04 +0900

    1부터 100까지 재귀로 호출해보기

--- src/main/java/com/likelion/algorithm1122/Print1To100.java
+++ src/main/java/com/likelion/algorithm1122/Print1To100.java
@@ -1,20 +1,27 @@
 package com.likelion.algorithm1122;
 
 public class Print1To100 {
-
     public void printNum(int num){
+        System.out.println(num);
+        printNum(num);
+
+    }
+
+
+    public void printNum1(int num){
         if (num == 1){
             System.out.println(num);
         } else{
             System.out.println(num);
-            printNum(num-1);
+            printNum1(num-1);
         }
 
     }
 
     public static void main(String[] args) {
         Print1To100 print1To100 = new Print1To100();
-        print1To100.printNum(100);
+        print1To100.printNum1(100);
+        print1To100.printNum(1); //stackoverflow
 
     }
 }
