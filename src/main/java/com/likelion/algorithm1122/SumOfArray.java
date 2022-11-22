7168ffdf2b10a233c2643b10c0054ce75f48f628
Qkite <yeonjigu4@gmail.com>
2022-11-22 10:15:26 +0900

    재귀 - array 원소의 합 구하기 - 증가 조건 변경

--- src/main/java/com/likelion/algorithm1122/SumOfArray.java
+++ src/main/java/com/likelion/algorithm1122/SumOfArray.java
@@ -3,24 +3,18 @@ package com.likelion.algorithm1122;
 public class SumOfArray {
 
     public int summation (int[] arr, int idx){
-        int sum = 0;
 
-        if(idx >= arr.length){
-            return sum;
+        if (idx < 0){
+            return 0;
+        } else{
+            return arr[idx] + summation(arr, --idx) ;
         }
-        
-        sum += arr[idx];
-//            System.out.println(idx);
-        summation(arr, ++idx);
-        // idx ++ 로 하면 대입 후 증가함 -> 0에서 업데이트가 되지 않음
-        // ++idx를 이용해야 함
-
-        return sum;
     }
 
     public static void main(String[] args) {
         SumOfArray sumOfArray = new SumOfArray();
-        System.out.println(sumOfArray.summation(new int[] {7,3,2,9}, 0));
+        int[] arr = new int[] {7,3,2,9};
+        System.out.println(sumOfArray.summation(arr, arr.length-1));
         // stack over flow 에러..?
     }
 }
