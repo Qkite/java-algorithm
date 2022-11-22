380aa2500b69fd771efa02de52174dbc275973a6
Qkite <yeonjigu4@gmail.com>
2022-11-22 10:19:44 +0900

    재귀 - 리스트 원소의 합 - remove 이용하기

--- src/main/java/com/likelion/algorithm1122/SumOfList.java
+++ src/main/java/com/likelion/algorithm1122/SumOfList.java
@@ -5,15 +5,30 @@ import java.util.List;
 
 public class SumOfList {
 
-    public int summmation(List<Integer> list, int idx){
+    public int summation(List<Integer> list, int idx){
+        // index 이동하기
 
         if (idx < 0){
             return 0;
         } else{
             // return  summmation(list, --idx) + list.get(idx) ;
             // 를 하면 idx가 이미 업데이트가 된 상태이므로 업데이트가 된 idx가 list.get에 넘어감
-            return list.get(idx) + summmation(list, --idx) ;
+            return list.get(idx) + summation(list, --idx) ;
         }
+
+    }
+
+    public int summationWithRemove(List<Integer> list){
+        // remove 사용하기
+
+        if(list.isEmpty()){
+            return 0;
+        } else{
+            return list.remove(list.size()-1) + summationWithRemove(list);
+
+            // remove를 하면 stack의 pop 처럼 제거된 원소가 튀어나오게 됨
+        }
+
 
 
     }
@@ -26,7 +41,8 @@ public class SumOfList {
         list.add(2);
         list.add(9);
 
-        System.out.println(sumOfList.summmation(list, list.size()-1));
+        System.out.println(sumOfList.summation(list, list.size()-1));
+        System.out.println(sumOfList.summationWithRemove(list));
 
 
 
