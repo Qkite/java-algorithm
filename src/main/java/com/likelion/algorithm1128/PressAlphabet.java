9e285ab81680bb6ca80b8dae902edced7a93d6c4
Qkite <yeonjigu4@gmail.com>
2022-11-28 09:15:00 +0900

    알파벳 입력하기 - 2칸

--- src/main/java/com/likelion/algorithm1128/PressAlphabet.java
+++ src/main/java/com/likelion/algorithm1128/PressAlphabet.java
@@ -10,15 +10,25 @@ public class PressAlphabet {
         }
     }
 
-//    void twoLetter(){
-//
-//
-//    }
+    void twoLetter(){
+
+        for (int ascii1 = 65; ascii1 < 91; ascii1++) {
+
+            for (int ascii2 = 65; ascii2 < 91; ascii2++) {
+                System.out.printf("%c %c \n",(char) ascii1, (char) ascii2);
+            }
+
+
+        }
+
+    }
 
 
     public static void main(String[] args) {
         PressAlphabet pressAlphabet = new PressAlphabet();
         pressAlphabet.oneLetter();
+
+        pressAlphabet.twoLetter();
 
 
     }
