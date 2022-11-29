3343d4ebe4661348e273b57e5f40b64764ccb080
Qkite <yeonjigu4@gmail.com>
2022-11-29 10:21:09 +0900

    자릿수 중복제거해서 구하기

--- src/main/java/com/likelion/algorithm1129/RadixSort.java
+++ src/main/java/com/likelion/algorithm1129/RadixSort.java
@@ -1,39 +1,26 @@
 package com.likelion.algorithm1129;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class RadixSort {
 
-    int[] radixSort(int[] inputs){
-        // int[]의 개수는 값의 range가 무엇인지 알아야 배열할 수 있음
+    int[] radixSortOfOneDigit(int[] inputs){
+        // 한 자리 수 -> 0에서 9까지 10개의 자리가 필요함
 
-        int max = inputs[0];
-        int min = inputs[0];
-
-        for (int num: inputs) {
-            if(num > max){
-                max = num;
-            }
-            if(num < min){
-                min = num;
-            }
-        }
-
-        int range = max - min + 1;
-
-        int[] indexArray = new int[range];
+        int[] indexArray = new int[10];
         int[] result = new int[inputs.length];
 
-        Arrays.fill(indexArray, min-1);
+        Arrays.fill(indexArray, -1);
 
         for (int input: inputs) {
-            indexArray[input-min] = input;
+            indexArray[input] = input;
         }
 
         int index = 0;
 
         for (int num: indexArray) {
-            if(num > min-1){
+            if(num > -1){
                 result[index] = num;
                 index++;
             }
@@ -44,11 +31,34 @@ public class RadixSort {
     }
 
 
+    Set<Integer> calculateDigits(int[] inputs){
+        // N 자리 수
+        // 1의 자리로 배열 -> 10의 자리수로 배열 -> ... -> 10^(n-1)의 자리수로 배열
+        // 최댓값의 자리수 구하기
+
+        Set<Integer> digits = new HashSet<>();
+
+        for (int i = 0; i < inputs.length; i++) {
+            if(inputs[i] == 0) {
+                digits.add(1);
+            } else if (inputs[i] < 0) {
+                continue;
+            } else{
+                digits.add((int) (Math.floor(Math.log10(inputs[i])) + 1));
+            }
+
+        }
+
+        return digits;
+    }
+
+
     public static void main(String[] args) {
 
         RadixSort radixSort = new RadixSort();
-        System.out.println(Arrays.toString(radixSort.radixSort(new int[]{7,4,5,9,1,0})));
-        System.out.println(Arrays.toString(radixSort.radixSort(new int[]{7,4,5,9,1,0,10,-2})));
+        // 0이상의 정수에서만 사용할 수 있음
+        System.out.println(Arrays.toString(radixSort.radixSortOfOneDigit(new int[]{7,4,5,9,1,0})));
+        System.out.println(radixSort.calculateDigits(new int[]{7,4,5,9,1,0,10,12,16,15,255,0,5,1654}));
 
     }
 }
