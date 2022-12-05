3240423fc4d2f87d0cdbcbed3ae5587b248cf8e2
Qkite <yeonjigu4@gmail.com>
2022-12-05 10:47:47 +0900

    부모 idx를 넣으면 heap으로 완료 - 전체 정렬 진행중

--- src/main/java/com/likelion/algorithm1205/Heap.java
+++ src/main/java/com/likelion/algorithm1205/Heap.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Heap {
 
-    int[] makeHeap(int[] array, int input){
+    int[] makeHeap1(int[] array, int input){
 
         int[] result = new int[array.length+1];
         int idx = array.length;
@@ -39,48 +39,74 @@ public class Heap {
         System.out.printf("%d 의 자식 노드는 %d 와 %d 입니다.\n", parentIdx, childIdx1, childIdx2);
     }
 
-    int[] makeHeap2(int[] array){
+
+    int[] makeHeap2(int[] arr, int parentIdx){
+
+
+        int childIdx1 = parentIdx*2 +1;
+        int childIdx2 = parentIdx*2 +2;
+        int greaterIdx = parentIdx;
+
+        if(arr.length>childIdx1){
+            if (arr[childIdx1] > arr[greaterIdx]){
+                greaterIdx = childIdx1;
+            }
+
+        }
+
+        if(arr.length>childIdx2){
+            if (arr[childIdx2] > arr[greaterIdx]){
+                greaterIdx = childIdx2;
+            }
+
+        }
+
+
+        if(greaterIdx == childIdx1){
+            int temp = arr[parentIdx];
+            arr[parentIdx] = arr[childIdx1];
+            arr[childIdx1] = temp;
+
+        } else if (greaterIdx == childIdx2) {
+            int temp = arr[parentIdx];
+            arr[parentIdx] = arr[childIdx2];
+            arr[childIdx2] = temp;
+        }
+
+        if(parentIdx > 0){
+            if (arr[parentIdx] > arr[(parentIdx-1)/2]){
+                arr = makeHeap2(arr, (parentIdx-1)/2);
+            }
+
+        }
+
+        return arr;
+    }
+
+    int[] sort(int[] arr){
 
         // 부모 노드의 끝: 0*2, 1*2, 3*2, 7*2, 15*2,...
         // (2^depth -1)*2
 
-        int depth = (int) (Math.log(array.length-1)/Math.log(2) + 1);
+        int depth = (int) (Math.floor(Math.log((arr.length-1)/2)/Math.log(2))+1);
 
-
-        for (int i = 0; i <= (2*depth-1)*2 ; i++) {
-            int parentIdx = i;
-            int childIdx1 = parentIdx*2 +1;
-            int childIdx2 = parentIdx*2 +2;
-
-            if(parentIdx)
-
-
+        System.out.println("depth: " + depth);
+        for (int i = 0; i <= (Math.pow(2, depth)*2 - 2); i++) {
+            System.out.println("parentIdx" + i);
+            arr = makeHeap2(arr, i);
         }
 
-
-
-        result[array.length] = input;
-
-
-        while ((input > array[idx/2]) & (idx > 0)){
-            result[idx] = array[idx/2];
-            idx /= 2;
-
-            if(idx == 0){
-                result[idx] = input;
-
-            }
-        }
-
-        return result;
+        return arr;
     }
 
     public static void main(String[] args) {
 
         Heap heap = new Heap();
-        System.out.println(Arrays.toString(heap.makeHeap(new int[]{6, 5, 7}, 8)));
+        System.out.println(Arrays.toString(heap.makeHeap1(new int[]{6, 5, 7}, 8)));
         heap.convertToHeap(10);
         heap.convertToHeap(33);
+        System.out.println(Arrays.toString(heap.sort(new int[]{6, 5, 7, 8})));
+        System.out.println(Arrays.toString(heap.sort(new int[]{4,8,5,7,3,2,9,6,7})));
 
 
 
