cd1e8d9211ad7f7175ad3e3cc031483ee4b6f6cb
Qkite <yeonjigu4@gmail.com>
2022-12-05 10:11:33 +0900

    힙 - 부모노드 idx로 자식 노드 idx 구하기

--- src/main/java/com/likelion/algorithm1205/Heap.java
+++ src/main/java/com/likelion/algorithm1205/Heap.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Heap {
 
     int[] makeHeap(int[] array, int input){
+
         int[] result = new int[array.length+1];
         int idx = array.length;
 
@@ -25,7 +26,51 @@ public class Heap {
             }
         }
 
+        return result;
+    }
 
+    void convertToHeap(int parentIdx){
+
+        // 부모 idx 입력하면 자식 idx 출력
+
+        int childIdx1 = parentIdx*2 +1;
+        int childIdx2 = parentIdx*2 +2;
+
+        System.out.printf("%d 의 자식 노드는 %d 와 %d 입니다.\n", parentIdx, childIdx1, childIdx2);
+    }
+
+    int[] makeHeap2(int[] array){
+
+        // 부모 노드의 끝: 0*2, 1*2, 3*2, 7*2, 15*2,...
+        // (2^depth -1)*2
+
+        int depth = (int) (Math.log(array.length-1)/Math.log(2) + 1);
+
+
+        for (int i = 0; i <= (2*depth-1)*2 ; i++) {
+            int parentIdx = i;
+            int childIdx1 = parentIdx*2 +1;
+            int childIdx2 = parentIdx*2 +2;
+
+            if(parentIdx)
+
+
+        }
+
+
+
+        result[array.length] = input;
+
+
+        while ((input > array[idx/2]) & (idx > 0)){
+            result[idx] = array[idx/2];
+            idx /= 2;
+
+            if(idx == 0){
+                result[idx] = input;
+
+            }
+        }
 
         return result;
     }
@@ -34,6 +79,10 @@ public class Heap {
 
         Heap heap = new Heap();
         System.out.println(Arrays.toString(heap.makeHeap(new int[]{6, 5, 7}, 8)));
+        heap.convertToHeap(10);
+        heap.convertToHeap(33);
+
+
 
     }
 }
