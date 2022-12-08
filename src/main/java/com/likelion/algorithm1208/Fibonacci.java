12fa8577397d0e11dc1c672d42e7deb798dd4efc
Qkite <yeonjigu4@gmail.com>
2022-12-08 09:39:04 +0900

    피보나치 수열 - DP 이용하기 - 재귀를 돌릴 때 메모를 같이 넘겨줌

--- src/main/java/com/likelion/algorithm1208/Fibonacci.java
+++ src/main/java/com/likelion/algorithm1208/Fibonacci.java
@@ -19,13 +19,36 @@ public class Fibonacci {
             }
         }
 
-        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
 
         // return arr; -> 수열 전체를 print
 
 
         return arr[nth-1];
     }
+
+    // DP - 재귀를 쓰지만 arr를 같이 넘김
+
+    int fibonacciWithDP(int num, int[] arr){
+
+        if(num <=2){
+            arr[num-1] = 1;
+            return 1;
+        } else{
+            if(arr[num-1]==0){
+                arr[num-1] = fibonacciWithDP(num-1, arr) + fibonacciWithDP(num-2, arr);
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+
+        return arr[num-1];
+
+
+    }
+
+
 
 
 
@@ -35,6 +58,7 @@ public class Fibonacci {
         Fibonacci fibonacci = new Fibonacci();
 
         System.out.println(fibonacci.fibonacci(num, new int[num]));
+        System.out.println(fibonacci.fibonacciWithDP(num, new int[num]));
 
     }
 }
