ae37541ea1024ba78454dcfda00cd60a213cfa90
Qkite <yeonjigu4@gmail.com>
2022-12-12 09:45:10 +0900

    최소 비용 구하기 - 최소 비용 구하기

--- src/main/java/com/likelion/algorithm1212/MinCost.java
+++ src/main/java/com/likelion/algorithm1212/MinCost.java
@@ -53,6 +53,7 @@ public class MinCost {
     public static void main(String[] args) {
 
         MinCost minCost = new MinCost();
+        System.out.println(minCost.getMinCost(new int[][]{{1,3,2},{4,6,2},{1,2,4}}));
         System.out.println(minCost.getMinCost(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
         System.out.println(minCost.getMinCost(new int[][]{{1}}));
         System.out.println(minCost.getMinCost(new int[][]{{1,2,3}}));
