036f6267765ce8c732d8c800a07e98f8f35d9069
Qkite <yeonjigu4@gmail.com>
2022-12-13 10:51:58 +0900

    코인들의 합 구하기

---
+++ src/main/java/com/likelion/algorithm1213/OptimalStrategy2.java
@@ -0,0 +1,45 @@
+package com.likelion.algorithm1213;
+
+import java.util.Arrays;
+
+public class OptimalStrategy2 {
+
+    int[][] sumOfCoins(int[] coins){
+
+        int[][] sumArray = new int[coins.length][coins.length];
+
+        for (int i = 0; i < coins.length; i++) {
+            sumArray[i][i] = coins[i];
+        }
+
+
+        for (int i = 0; i < coins.length-1; i++) {
+            sumArray[i][i+1] = sumArray[i][i] + sumArray[i+1][i+1];
+
+        }
+
+        for (int i = 0; i < coins.length-2; i++) {
+            sumArray[i][i+2] = sumArray[i][i+1] + sumArray[i+1][i+2];
+
+        }
+
+        for (int i = 0; i < coins.length-3; i++) {
+            sumArray[i][i+3] = sumArray[i][i+2] + sumArray[i+1][i+3];
+
+        }
+
+
+        System.out.println(Arrays.deepToString(sumArray));
+
+        return sumArray;
+
+
+    }
+
+    public static void main(String[] args) {
+        OptimalStrategy2 optimalStrategy2 = new OptimalStrategy2();
+        int[] coins = new int[]{2,7,40,19};
+        System.out.println(Arrays.deepToString(optimalStrategy2.sumOfCoins(coins)));
+
+    }
+}
