c56c337f417f98a419a3cc000611af738e77b298
Qkite <yeonjigu4@gmail.com>
2022-12-13 09:51:48 +0900

    최적 전략 구하기 - 2개가 있는 경우

--- src/main/java/com/likelion/algorithm1213/OptimalStrategy.java
+++ src/main/java/com/likelion/algorithm1213/OptimalStrategy.java
@@ -19,14 +19,37 @@ public class OptimalStrategy {
 
         Pair[][] result = new Pair[input.length][input.length];
 
+        // 1개 씩만 있는 경우
+
         for (int i = 0; i < input.length; i++) {
             result[i][i] = new Pair(input[i], 0);
         }
+
+        // 2개가 있는 경우
+
+        for (int i = 0; i < input.length-1; i++) {
+            int left1 = input[i];
+            int left2 = input[i+1];
+            if(left1>= left2){
+                result[i][i+1] = new Pair(input[i], input[i+1]);
+            } else{
+                result[i][i+1] = new Pair(input[i+1], input[i]);
+            }
+
+
+        }
+
+
 
         for (int i = 0; i < result.length; i++) {
 
             System.out.printf("%d %d, left: %d, right: %d \n", i, i, result[i][i].left, result[i][i].right);
 
+
+        }
+
+        for (int i = 0; i < result.length-1; i++) {
+            System.out.printf("%d %d, left: %d, right: %d \n", i, i+1, result[i][i+1].left, result[i][i+1].right);
         }
 
         return result;
