621cbab5a053b73fb48cdfa2ef907e52a1ddfd97
Qkite <yeonjigu4@gmail.com>
2022-12-14 09:35:05 +0900

    이차원 배열 원소들의 합 구하기 - DP 이용하기

--- src/main/java/com/likelion/algorithm1214/FromToSum.java
+++ src/main/java/com/likelion/algorithm1214/FromToSum.java
@@ -3,8 +3,10 @@ package com.likelion.algorithm1214;
 import java.util.Arrays;
 
 public class FromToSum {
+
+    // 2중(3중) 반복문
     
-    int[][] fromToSum(int[] input){
+    int[][] fromToSum1(int[] input){
         
         int[][] result = new int[input.length][input.length];
 
@@ -21,10 +23,36 @@ public class FromToSum {
         return result;
     }
 
+    // 배열에 기록하기
+
+    int[][] fromToSum2(int[] input){
+
+        int[][] result = new int[input.length][input.length];
+
+        // 0-0,1-1,2-2
+        // 0-1,1-2
+        // 0-2
+
+        // 첫째줄
+        for (int i = 0; i < input.length; i++) {
+            result[i][i] = input[i];
+        }
+
+        // 둘째줄
+        for (int i = 0; i < input.length-1; i++) {
+            result[i][i+1] = result[i][i] + result[i+1][i+1];
+
+        }
+
+
+        return result;
+    }
+
     public static void main(String[] args) {
 
         FromToSum fromToSum = new FromToSum();
-        System.out.println(Arrays.deepToString(fromToSum.fromToSum(new int[]{1, 3, 5, 4, 6, 7})));
+        System.out.println(Arrays.deepToString(fromToSum.fromToSum1(new int[]{1, 3, 5, 4, 6, 7})));
+        System.out.println(Arrays.deepToString(fromToSum.fromToSum2(new int[]{1, 3, 5, 4, 6, 7})));
         
     }
 }
